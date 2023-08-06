40af271f99a4b2f30e31b5eb2cacf8b45670c400
Maksym Pedko <maksim.pedko@yalantis.net>
2023-08-06 18:57:19 +0300

    [add] images

--- GTAModes/GTAModes/Flows/MainFlow/GameModes/Views/Cell/GameModesTableViewCell.swift
+++ GTAModes/GTAModes/Flows/MainFlow/GameModes/Views/Cell/GameModesTableViewCell.swift
@@ -22,7 +22,8 @@ final class GameModesTableViewCell: UITableViewCell, Reusable {
     private let titleLabel = UILabel()
     private let favoriteImage = UIImageView()
     private let stackView = UIStackView()
-    private let contentView = UIView()
+    private let contentModeView = UIView()
+    private let modeTitleLabel = UILabel()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -39,7 +40,18 @@ final class GameModesTableViewCell: UITableViewCell, Reusable {
         titleLabel.font = UIFont(name: "Inter-Regular", size: 20)
         titleLabel.textColor = .white
         titleLabel.text = value.title
-        favoriteImage.image = UIImage()
+        favoriteImage.image =  UIImage(named: value.isFavorite ? "favoriteYesIcon" : "favoriteNoIcon")
+        if value.imageNames.count > 1 {
+            contentModeView.isHidden = true
+            stackView.isHidden = false
+            addImages(value.imageNames)
+        } else {
+            contentModeView.isHidden = false
+            stackView.isHidden = true
+            modeTitleLabel.text = value.imageNames.first ?? ""
+            modeTitleLabel.font = UIFont(name: "Inter-Regular", size: 15)
+            modeTitleLabel.textColor = .white
+        }
     }
     
     private func setupLayout() {
@@ -62,25 +74,39 @@ final class GameModesTableViewCell: UITableViewCell, Reusable {
             $0.height.equal(to: 36.0)
             $0.width.equal(to: 36.0)
         }
-        favoriteImage.image = UIImage(named: "favoriteIcon")
+        favoriteImage.image = UIImage(named: "favoriteNoIcon")
         
         containerView.addSubview(titleLabel)
         titleLabel.layout {
             $0.leading.equal(to: containerView.leadingAnchor, offsetBy: 4.0)
-            $0.trailing.greaterThanOrEqual(to: swicher.leadingAnchor, offsetBy: -4.0)
+            $0.trailing.greaterThanOrEqual(to: favoriteImage.leadingAnchor, offsetBy: -4.0)
             $0.top.equal(to: containerView.topAnchor, offsetBy: 12.0)
         }
         
+        containerView.addSubview(contentModeView)
+        contentModeView.layout {
+            $0.leading.equal(to: containerView.leadingAnchor)
+            $0.trailing.greaterThanOrEqual(to: containerView.leadingAnchor)
+            $0.bottom.equal(to: containerView.bottomAnchor, offsetBy: -12.0)
+            $0.height.equal(to: 24.0)
+        }
+        
+        contentModeView.addSubview(modeTitleLabel)
+        modeTitleLabel.layout {
+            $0.leading.equal(to: contentModeView.leadingAnchor, offsetBy: 4.0)
+            $0.trailing.equal(to: contentModeView.trailingAnchor, offsetBy: -4.0)
+            $0.centerY.equal(to: contentModeView.centerYAnchor)
+        }
     }
     
-    private func addImages(_ images: [UIImage()]) {
+    private func addImages(_ imagesName: [String]) {
         stackView.axis = .horizontal
         stackView.spacing = 8 // Adjust the spacing between images as needed
         stackView.distribution = .fillEqually // Adjust distribution mode as needed
         stackView.alignment = .center
         
-        for image in images {
-            let imageView = UIImageView(image: image)
+        for image in imagesName {
+            let imageView = UIImageView(image: UIImage(named: image))
             imageView.contentMode = .scaleAspectFit
             imageView.translatesAutoresizingMaskIntoConstraints = false
             imageView.widthAnchor.constraint(equalToConstant: 24).isActive = true
@@ -90,8 +116,14 @@ final class GameModesTableViewCell: UITableViewCell, Reusable {
         }
         
         // Add the stack view to the main view
-        view.addSubview(stackView)
+        contentModeView.addSubview(stackView)
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(v)
+        stackView.layout {
+            $0.leading.equal(to: containerView.leadingAnchor)
+            $0.trailing.greaterThanOrEqual(to: containerView.leadingAnchor)
+            $0.bottom.equal(to: containerView.bottomAnchor, offsetBy: -12.0)
+            $0.height.equal(to: 24.0)
     }
     
 }
--- GTAModes/GTAModes/Flows/MainFlow/GameModes/Views/Header/GameModesHeaderView.swift
+++ GTAModes/GTAModes/Flows/MainFlow/GameModes/Views/Header/GameModesHeaderView.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import UIKit
+
+final class GameModesHeaderView: UITableViewHeaderFooterView, Reusable {
+  
+  static let height = 32.0
+  
+  private let titleLabel = UILabel()
+  
+  override init(reuseIdentifier: String?) {
+    super.init(reuseIdentifier: reuseIdentifier)
+    
+    setupView()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func setTitle(_ title: NSAttributedString) {
+    titleLabel.attributedText = title
+  }
+  
+  private func setupView() {
+    titleLabel.accessibilityIdentifier = "titleLabel"
+    contentView.addSubviews(titleLabel)
+    titleLabel.layout {
+      $0.centerY.equal(to: centerYAnchor)
+      $0.leading.equal(to: contentView.leadingAnchor, offsetBy: .layoutMargin2x)
+    }
+  }
+  
+}
