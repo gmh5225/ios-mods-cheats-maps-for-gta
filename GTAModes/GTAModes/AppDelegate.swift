eb3c75f0593702fb20938018eca6bc61ab49a5dc
Alexander Malinchuk <a.malinchuk@gmail.com>
2023-08-10 11:50:40 +0300

    - DropBox service fixes

--- GTAModes/GTAModes/AppDelegate.swift
+++ GTAModes/GTAModes/AppDelegate.swift
@@ -15,8 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        DBManager().setupDropBox()
-        DBManager().fetchGTA5Modes()
+        DBManager.shared.setupDropBox()
         applicationFlowCoordinator = ApplicationFlowCoordinator()
         
         return true
